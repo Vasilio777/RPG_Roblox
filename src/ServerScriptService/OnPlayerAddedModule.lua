7c35277d047bb471efd61f19e73503482e952f2b
v.kotunov <purgen200@gmail.com>
2024-05-15 18:43:13 +0700

    removed RND; added ServerStorage for sync; added Coins implementation from dev docs

--- src/ServerScriptService/OnPlayerAddedModule.lua
+++
@@ -1,9 +0,0 @@
-local OnPlayerAddedModule = {}
-
-local LeaderStatsModule = require(game.ServerScriptService.LeaderStatsModule)
-
-game.Players.PlayerAdded:Connect(function(new_player)
-	LeaderStatsModule.OnPlayerAdded(new_player)
-end)
-
-return OnPlayerAddedModule
---
+++ src/ServerScriptService/OnPlayerAddedModule.server.lua
@@ -0,0 +1,8 @@
+local OnPlayerAddedModule = {}
+
+
+game.Players.PlayerAdded:Connect(function(new_player)
+	
+end)
+
+return OnPlayerAddedModule
