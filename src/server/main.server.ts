6a951a258bde5008373fd38d139feac912f0f309
v.kotunov <purgen200@gmail.com>
2024-05-17 18:56:34 +0700

    added rbxts/net

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,4 +1,3 @@
 import { makeHello } from "shared/module";
 
 print(makeHello("main.server.ts"));
-print(makeHello("I need to do it twice"));
---
+++ src/shared/remotes.ts
@@ -0,0 +1,7 @@
+import Net from "@rbxts/net";
+
+const Remotes = Net.CreateDefinitions({
+	// Definitions for the actual remotes will go here
+});
+
+export = Remotes;
